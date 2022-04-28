be8b1fc8fe20448154ae2fcbee2eb096431cc0db
opheliah95 <opheliah95@gmail.com>
2022-04-28 16:28:54 +0100

    initial exercise with tuple

--- src/main/java/com/virtualpairprogrammers/Tuple.java
+++ src/main/java/com/virtualpairprogrammers/Tuple.java
@@ -6,19 +6,23 @@ import java.util.List;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
+import scala.Tuple2;
 
 public class Tuple {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		List<Integer> inputData = new ArrayList<Integer>();
-		inputData.add(3);
-		inputData.add(12);
-		inputData.add(50);
-		inputData.add(20);
+		List<String> inputData = new ArrayList<>();
+		inputData.add("WARN: Tuesday 4 September 0405");
+		inputData.add("ERROR: Tuesday 4 September 0408");
+		inputData.add("FATAL: Wednesday 5 September 1632");
+		inputData.add("ERROR: Friday 7 September 1854");
+		inputData.add("WARN: Saturday 8 September 1942");
+		
 		
 		// configure logging so there is not too much red
 		/// remove unnecessary log from apache
@@ -29,20 +33,26 @@ public class Tuple {
 		JavaSparkContext sc = new JavaSparkContext(conf);
 		
 		// load data into spark -> a scalar wrapup
-		JavaRDD<Integer> myRdd = sc.parallelize(inputData);
+		JavaRDD<String> originalMessages = sc.parallelize(inputData);
 		
-		// reduces rdd
-		JavaRDD<Double> sqrtRdd= myRdd.map(var -> Math.sqrt(var) );
-
-		sqrtRdd.foreach(val -> System.out.println("the value is: " + val));
+		// create a pair RDD
+		JavaPairRDD<String, String> pairRDD = originalMessages.mapToPair(rawValue -> {
+			String[] columns = rawValue.split(":");
+			String level = columns[0];
+			String message = columns[1];
+			return new Tuple2<String, String>(level, message);
+		});
 		
-		Long count = sqrtRdd.count();
-		System.out.println("total data in the set: " + count);
+		// do something with string
+		JavaPairRDD<String, Long> counterRDD = originalMessages.mapToPair(rawValue -> {
+			String[] columns = rawValue.split(":");
+			String level = columns[0];
+			return new Tuple2<String, Long>(level, 1L);
+		});
 		
-		// more spark native way to do the counting
-		JavaRDD<Integer> r = sqrtRdd.map(val -> 1);
-		Integer c = r.reduce((val1, val2) -> val1 + val2);
-		System.out.println("total count of interger is " + c);
+		JavaPairRDD<String, Long> keyCount = counterRDD.reduceByKey((a, b) -> a + b);
+		
+		keyCount.foreach(val -> System.out.println(val._1 + ":" + val._2));
 		sc.close();
 
 	}
