0f39345fbaf0f831953c6e96807fe6d54eeb386c
opheliah95 <opheliah95@gmail.com>
2022-04-28 16:28:39 +0100

    added customized class

--- src/main/java/com/virtualpairprogrammers/IntWithSqrt.java
+++ src/main/java/com/virtualpairprogrammers/IntWithSqrt.java
@@ -1,5 +1,14 @@
 package com.virtualpairprogrammers;
 
 public class IntWithSqrt {
+	
+	private int originalNumber;
+	private double squareRoot;
+
+	public IntWithSqrt(int i) {
+		// TODO Auto-generated constructor stub
+		this.originalNumber = 1;
+		this.squareRoot = Math.sqrt(originalNumber);
+	}
 
 }
